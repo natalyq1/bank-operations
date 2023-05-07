ea76ee2d4250353bfdcc93933e48e8cbc21c8b76
natalyq1 <onatylili@gmail.com>
2023-05-07 14:22:24 -0500

    refactoring

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
+import { Icono } from "../UI";
 
 const Account = () => {
   const [toggleState, untoggle] = useState(true);
@@ -17,7 +18,7 @@ const Account = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
         <span>
-          <img className="imagen-icono" src={dinero} alt="Icono de saldo" />
+          <Icono src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
           <div className="saldo">
@@ -27,9 +28,8 @@ const Account = () => {
       </div>
 
       <button className="btn" onClick={toggleHandler}>
-        <img
+        <Icono
           style={{ marginTop: "2px" }}
-          className="imagen-icono"
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import styled from 'styled-components'
 
 import Title from "../Title";
 import Account from "../Account";
 
+const StyleContainer = styled.div `
+background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyleContent = styled.section `
+display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+/**const = styled. `` */
+
 const Container = () => {
   return (
-    <div className="container">
+    <StyleContainer>
       <Title>Smart Bank</Title>
-      <section className="content">
+      <StyleContent>
         <Account />
-      </section>
-    </div>
+      </StyleContent>
+    </StyleContainer>
   );
 };
 
