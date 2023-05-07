a6a26bece15029ec92e579e33afda9c5920db38d
natalyq1 <onatylili@gmail.com>
2023-05-07 18:43:57 -0500

    Dark theme and Light theme added

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
-import { Box, Btn, Detalle, Icono, Saldo } from "../UI";
+import { Box, Btn, Detalle, Icono, Saldo, IconoTema } from "../UI";
 
 const IconoMargin = styled(Icono) `
 margin-top: 2px;
@@ -25,7 +25,7 @@ const Account = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
         <Detalle>
-          <Icono src={dinero} alt="Icono de saldo" />
+          <IconoTema src={dinero} alt="Icono de saldo" />
         </Detalle>
         {toggleState ? (
           <Saldo>
--- src/Components/UI/variables.js
+++ src/Components/UI/variables.js
@@ -1,4 +1,9 @@
-export const colorPrimario = '#41d3be'
-export const fondoClaro = '#f1f1f1'
-export const textoFondoClaro = 'gray'
+export const colorPrimario = "#41D3BE";
 
+export const fondoClaro = "#F1F1F1";
+export const contenidoClaro = "white";
+export const textoFondoClaro = "grey";
+
+export const fondoOscuro = "#363537";
+export const contenidoOscuro = "#5C5B5E";
+export const textoFondoOscuro = "#FAFAFA";
