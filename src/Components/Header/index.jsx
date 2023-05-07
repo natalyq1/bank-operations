1253bd4c1e3369e41545ff7251abb9af800e2402
natalyq1 <onatylili@gmail.com>
2023-05-07 13:57:27 -0500

    styled-components with variables

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import styled from 'styled-components'
 import logo from "../../assets/images/logo.svg";
+import { colorPrimario } from "../UI/variables";
 
 const StyledHeader = styled.nav `
-background-color: #41d3be;
+background-color: ${colorPrimario};
   display: flex;
   justify-content: space-between;
   padding: 0 15vw;
   height: 10vh;
   align-items: center;
   `
+const Logo =styled.img `
+height: 50px;
+  width: 50px;
+`
 
 const Header = () => {
   return (
     <StyledHeader>
-      <img className="imagen-logo" src={logo} alt="Logo Smart Bank" />
+      <Logo src={logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secundario" href="https://google.com">
           Ayuda
